2b885537118d4aa9aa1d795bc6038125a31373a4
aaronparisi <parisi.aaron@gmail.com>
2023-07-27 10:38:56 -0700

    file case issues

---
+++ src/components/SlideOnClick.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useSpring, animated, config } from 'react-spring';
+
+const SlideOnClick: React.FC = () => {
+  const [slideSpring, slideApi] = useSpring(() => {
+    return {
+      from: { x: 100 },
+    };
+  });
+
+  const handleClick = () => {
+    console.log('user clicked on SlideOnClick; sliding now');
+    slideApi.start({
+      from: {
+        x: 100,
+      },
+      to: {
+        x: 500,
+      },
+      config: config.wobbly,
+    });
+  };
+
+  return (
+    <section className="pane">
+      <section className="animation-window">
+        <animated.div
+          className="slide-on-click springy"
+          onClick={handleClick}
+          style={slideSpring}
+        ></animated.div>
+      </section>
+      <p>
+        Click events are handled by a function that calls `api.start`
+        explicitly. The `to` and `from` values are defined in start's config
+        object argument. Subsequent clicks <em>do not</em> "reverse" the
+        animation.
+      </p>
+    </section>
+  );
+};
+
+export default SlideOnClick;
